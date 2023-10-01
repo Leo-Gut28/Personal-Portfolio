75bcb787fe0cb2669f5cfac7e5523e25610b0db6
Leonardo Gutierrez <leonardo.gutierrez@duke.edu>
2023-10-01 18:39:17 -0400

    Updates

--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'leonardo.gutierrez2804@gmail.com',
-    phone: '703-675-7049',
-    address: '5437 New London Park Dr. Fairfax, VA 22032 ',
-    github: 'https://github.com/Leo-Gut28',
-    linkedIn: 'https://www.linkedin.com/in/leonardo-gutierrez-37b898208/',
-}
