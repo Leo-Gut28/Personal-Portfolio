e18d090197820052993adc3650e1aafa565c7ff1
Leonardo Gutierrez <leonardo.gutierrez@duke.edu>
2023-10-01 20:06:06 -0400

    Revert "Updates"
    This reverts commit 7b111f5dc35c6359fd4417ce02b3996e817d6fb5.

---
+++ src/components/container/pokerandom-container.js
@@ -0,0 +1,59 @@
+import { makeStyles } from '@mui/styles';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../contexts/theme-context';
+import { HashLink as NavLink } from 'react-router-hash-link';
+import PokerandomUI from '../core-ui/arduinorobot/arduinorobot';
+
+
+const Pokerandom = ({ to, label, }) => {
+  const { theme } = useContext(ThemeContext);
+
+  const useStyles = makeStyles((t) => ({
+    contactBtn: {
+      backgroundColor: theme.primary,
+      color: theme.secondary,
+      borderRadius: '30px',
+      textTransform: 'inherit',
+      textDecoration: 'none',
+      width: '150px',
+      height: '50px',
+      fontSize: '1rem',
+      fontWeight: '500',
+      fontFamily: 'var(--primaryFont)',
+      border: `3px solid ${theme.primary}`,
+      transition: '500ms ease-out',
+      '&:hover': {
+        backgroundColor: theme.buttonColor,
+        color: theme.secondary,
+        border: `3px solid ${theme.buttonColor}`,
+      },
+      [t.breakpoints.down('sm')]: {
+        display: 'none',
+      },
+    },
+    drawerLinks: {
+        fontFamily: 'var(--primaryFont)',
+        width: '50%',
+        fontSize: '1.3rem',
+        fontWeight: 600,
+        [t.breakpoints.down('sm')]: {
+          fontSize: '1.125rem',
+        },
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <NavLink to={to} smooth={true} spy={true} duration={2000}>
+        <span className={classes.drawerLinks}>
+            <button className={classes.contactBtn}>{label}</button>
+        </span>
+    </NavLink>
+    </div>
+    
+  );
+};
+
+export default Pokerandom;
---
+++ src/components/core-ui/pokerandom/pokerandom.js
@@ -0,0 +1,68 @@
+
+import React, { useContext } from 'react';
+import './pokerandom.css';
+import pokepicture from '../../../assets/png/PokeRandom.gif';
+import pokepart1 from '../../../assets/png/Part1Poke.gif';
+import pokepart2 from '../../../assets/png/Part2Poke.gif';
+import { Container } from '@mui/material';
+import { ThemeContext } from '../../../../src/contexts/theme-context';
+import { pokerandomData } from '../../../data/pokerandomData';
+import { Helmet } from 'react-helmet'
+import Pokerandom from '../../../../src/components/container/pokerandom-container';
+
+
+
+function PokerandomUI() {
+
+    const { theme } = useContext(ThemeContext);
+    return (
+        
+        <div style={{ backgroundColor: theme.secondary }}>
+            <Helmet>
+                <title> Leonardo Gutierrez - Porfolio</title>
+            </Helmet>
+            <Container className="arduinorobot" id="arduinorobot">
+                <div className="line-styling">
+                    <div className="style-circle" style={{ backgroundColor: theme.primary }}></div>
+                    <div className="style-circle" style={{ backgroundColor: theme.primary }}></div>
+                    <div className="style-line" style={{ backgroundColor: theme.primary }}></div>
+                </div>
+                <div className="about-body">
+                    <div className="about-description">
+                        <h2 style={{ color: theme.primary }}>{pokerandomData.title}</h2>
+                        <p style={{ color: theme.tertiary }}>{pokerandomData.description1}<br /><br />{pokerandomData.description2}</p>
+                    </div>
+                    <div className="about-animation">
+                    <img src={pokepicture} alt="ANIMATED GIF" width="400" height="600"/>
+                    </div>
+                </div>
+                <div className = "sensors">
+                    <div className = "about-description">
+                    <h3 style={{ color: theme.primary }}>{pokerandomData.sensordiagrams}</h3>
+                        <h4 style={{ color: theme.tertiary }}>{pokerandomData.description3}</h4>
+                        <p style={{ color: theme.tertiary }}>{pokerandomData.description4}</p>
+                        <div className="about-animation">
+                            <img src={pokepart1} alt="" width="400" height="600"/>
+                        </div>
+                        <br /><br />
+                        <h4 style={{ color: theme.tertiary }}>{pokerandomData.description5}</h4>
+                        <p style={{ color: theme.tertiary }}>{pokerandomData.description6}</p>
+                        <div className="about-animation">
+                            <img src={pokepart2} alt="" width="400" height="600"/>
+                        </div>
+                        <br /><br />
+                        <h4 style={{ color: theme.tertiary }}>{pokerandomData.description7}</h4>
+                        <p style={{ color: theme.tertiary }}>{pokerandomData.description8}</p>
+                    </div>
+                </div>
+
+                <div className="back-button">
+                    <Pokerandom to="/#education" label="Back to Projects" />
+                    </div>
+
+            </Container>
+        </div>
+    );
+}
+
+export default PokerandomUI;
