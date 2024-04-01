daef8ed55c416cc3eefa9d5a392cf1d9cde926e5
Leonardo Gutierrez <leonardo.gutierrez@duke.edu>
2024-04-01 00:22:55 -0400

    updated intro

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import headshot from "../assets/png/Headshot.png"
 export const headerData = {
     name: 'Leonardo Gutierrez',
     title: "Hello! I am",
-    desciption: "Hi! I'm Leo! A quick learner and a fun-loving enthusiast. I'm always eager to acquire new skills and put them to work in the real world. I'm currently looking for internship opportunities for Summer 2024! If you have any questions, please contact me below!",
+    desciption: "Hi! I'm Leo! A quick learner and a fun-loving enthusiast with a passion for both hardware and software engineering. I'm always eager to acquire new skills and put them to work in the real world. If you would like to reach out, please contact me below!",
     image: headshot,
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
     resumePdf: 'https://drive.google.com/file/d/1QUSgExHMHWBjESR0T4I7hKK1WE2laPAj/view?usp=sharing'
