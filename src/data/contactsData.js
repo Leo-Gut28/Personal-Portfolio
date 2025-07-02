bef882a75a8d62f038fa2e609646dccc208c2461
Leonardo Gutierrez <leonardo.gutierrez@duke.edu>
2025-07-02 10:31:57 -0400

    updated basic info

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'leonardo.gutierrez2804@gmail.com',
-    phone: '703-675-7049',
+    phone: 'Available Ipon Request',
     address: 'Fairfax, VA',
     github: 'https://github.com/Leo-Gut28',
     linkedIn: 'https://www.linkedin.com/in/leonardo-gutierrez-ldg21/',
